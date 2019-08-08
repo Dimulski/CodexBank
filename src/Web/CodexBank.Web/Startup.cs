e8f348a4095da5b74646fb27191d7da92d9555a3
George <prophet.codex@gmail.com>
2019-08-08 11:40:06 +0300

    Update Startup

--- src/Web/CodexBank.Web/Startup.cs
+++ src/Web/CodexBank.Web/Startup.cs
@@ -16,7 +16,15 @@ using Microsoft.Extensions.DependencyInjection;
 using CodexBank.Data;
 using CodexBank.Models;
 using CodexBank.Data.Seeding;
-using CodexBank.Data.Extensions;
+using CodexBank.Web.Infrastructure.Extensions;
+using Microsoft.AspNetCore.Routing;
+using CodexBank.Common.Configuration;
+using CodexBank.Common.EmailSender;
+using CodexBank.Common.Utils;
+using AutoMapper;
+using CodexBank.Common.AutoMapping.Profiles;
+using CodexBank.Web.Infrastructure.Middleware;
+using Microsoft.Net.Http.Headers;
 
 namespace CodexBank.Web
 {
@@ -36,37 +44,108 @@ namespace CodexBank.Web
             {
                 // This lambda determines whether user consent for non-essential cookies is needed for a given request.
                 options.CheckConsentNeeded = context => true;
-                options.MinimumSameSitePolicy = SameSiteMode.None;
+                options.MinimumSameSitePolicy = Microsoft.AspNetCore.Http.SameSiteMode.None;
             });
 
-            services.AddDbContext<CodexBankDbContext>(options =>
+            services.AddDbContextPool<CodexBankDbContext>(options =>
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
 
-            services.AddScoped<BankUserRoleSeeder>();
-            services.AddScoped<BankAdminUserSeeder>();
+            services
+                .Configure<CookieTempDataProviderOptions>(options => { options.Cookie.IsEssential = true; });
 
             services.AddIdentity<BankUser, IdentityRole>(options =>
             {
-                options.Password.RequireDigit = false;
-                options.Password.RequireLowercase = false;
                 options.Password.RequireNonAlphanumeric = false;
-                options.Password.RequireUppercase = false;
-                options.Password.RequiredLength = 3;
-                options.Password.RequiredUniqueChars = 0;
+                options.SignIn.RequireConfirmedEmail = true;
 
-                options.User.RequireUniqueEmail = true;
+                options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(10);
+                options.Lockout.MaxFailedAccessAttempts = 5;
+                options.Lockout.AllowedForNewUsers = true;
             })
                 .AddEntityFrameworkStores<CodexBankDbContext>()
                 .AddDefaultTokenProviders();
 
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.ConfigureApplicationCookie(options =>
+            {
+                options.Cookie.HttpOnly = true;
+                options.ExpireTimeSpan = TimeSpan.FromMinutes(5);
+                options.SlidingExpiration = true;
+                options.LoginPath = "/account/login";
+                options.LogoutPath = "/account/logout";
+            });
+
+            services
+                .AddDomainServices()
+                .AddApplicationServices()
+                .AddCommonProjectServices()
+                .AddAuthentication();
+
+            services.Configure<SecurityStampValidatorOptions>(options => { options.ValidationInterval = TimeSpan.Zero; });
+
+            services.Configure<RouteOptions>(options => options.LowercaseUrls = true);
+
+            services
+                .Configure<BankConfiguration>(
+                    this.Configuration.GetSection(nameof(BankConfiguration)))
+                .Configure<SendGridConfiguration>(
+                    this.Configuration.GetSection(nameof(SendGridConfiguration)));
+
+            services
+                .PostConfigure<BankConfiguration>(settings =>
+                {
+                    if (!ValidationUtil.IsObjectValid(settings))
+                    {
+                        throw new ApplicationException("BankConfiguration is invalid.");
+                    }
+                })
+                .PostConfigure<SendGridConfiguration>(settings =>
+                {
+                    if (!ValidationUtil.IsObjectValid(settings))
+                    {
+                        throw new ApplicationException("SendGridConfiguration is invalid.");
+                    }
+                });
+
+            services
+                .AddResponseCompression(options => options.EnableForHttps = true);
+
+            services.AddMvc(options => { options.Filters.Add<AutoValidateAntiforgeryTokenAttribute>(); })
+                .AddRazorPagesOptions(options => { options.Conventions.AuthorizePage("/Transactions"); })
+                .SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            //services.AddScoped<BankUserRoleSeeder>();
+            //services.AddScoped<BankAdminUserSeeder>();
+
+            //services.AddIdentity<BankUser, IdentityRole>(options =>
+            //{
+            //    options.Password.RequireDigit = false;
+            //    options.Password.RequireLowercase = false;
+            //    options.Password.RequireNonAlphanumeric = false;
+            //    options.Password.RequireUppercase = false;
+            //    options.Password.RequiredLength = 3;
+            //    options.Password.RequiredUniqueChars = 0;
+
+            //    options.User.RequireUniqueEmail = true;
+            //})
+            //    .AddEntityFrameworkStores<CodexBankDbContext>()
+            //    .AddDefaultTokenProviders();
+
+            //services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            app.UseDatabaseSeeding();
+            using (var serviceScope = app.ApplicationServices.CreateScope())
+            {
+                using (var context = serviceScope.ServiceProvider.GetRequiredService<CodexBankDbContext>())
+                {
+                    context.Database.EnsureCreated();
+                }
+            }
+
+            Mapper.Initialize(config => config.AddProfile<DefaultProfile>());
 
             if (env.IsDevelopment())
             {
@@ -75,10 +154,59 @@ namespace CodexBank.Web
             }
             else
             {
-                app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseExceptionHandler("/error/500");
                 app.UseHsts();
             }
+
+            app.AddDefaultSecurityHeaders(
+                new SecurityHeadersBuilder()
+                    .AddDefaultSecurePolicy());
+
+            app.UseResponseCompression();
+            app.UseStatusCodePages();
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles(
+                new StaticFileOptions
+                {
+                    OnPrepareResponse = ctx =>
+                    {
+                        const int cacheDurationInSeconds = 60 * 60 * 24 * 365; // 1 year
+                        ctx.Context.Response.Headers[HeaderNames.CacheControl] =
+                            $"public,max-age={cacheDurationInSeconds}";
+                    }
+                });
+            app.UseCookiePolicy();
+
+            app.UseAuthentication();
+
+            app.UseStatusCodePagesWithReExecute("/error/{0}");
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    "areas",
+                    "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+                routes.MapRoute(
+                    "default",
+                    "{controller=Home}/{action=Index}/{id?}");
+            });
+
+            app.InitializeDatabase();
+
+            //app.UseDatabaseSeeding();
+
+            //if (env.IsDevelopment())
+            //{
+            //    app.UseDeveloperExceptionPage();
+            //    app.UseDatabaseErrorPage();
+            //}
+            //else
+            //{
+            //    app.UseExceptionHandler("/Home/Error");
+            //    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            //    app.UseHsts();
+            //}
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
