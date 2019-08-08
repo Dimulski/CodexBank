eeaa3b94295f9dff5afd544f9ea10981348aa46d
George <prophet.codex@gmail.com>
2019-08-08 12:35:00 +0300

    Add ReCaptcha support

---
+++ src/Web/CodexBank.Web/Models/BaseReCaptchaModel.cs
@@ -0,0 +1,14 @@
+﻿using CodexBank.Web.Infrastructure.Attributes;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace CodexBank.Web.Models
+{
+    public abstract class BaseReCaptchaModel
+    {
+        [Required]
+        [ValidateReCaptcha]
+        [BindProperty(Name = "g-recaptcha-response")]
+        public string ReCaptchaResponse { get; set; }
+    }
+}
---
+++ src/Web/CodexBank.Web/TagHelpers/RecaptchaTagHelper.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using Microsoft.Extensions.Configuration;
+
+namespace CodexBank.Web.TagHelpers
+{
+    [HtmlTargetElement("recaptcha")]
+    public class ReCaptchaTagHelper : TagHelper
+    {
+        private readonly IConfiguration configuration;
+
+        public ReCaptchaTagHelper(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            var siteKey = this.configuration["ReCaptcha:SiteKey"];
+
+            output.TagName = "div";
+            output.Attributes.SetAttribute("class", "g-recaptcha");
+            output.Attributes.SetAttribute("data-sitekey", siteKey);
+
+            output.TagMode = TagMode.StartTagAndEndTag;
+        }
+    }
+}
