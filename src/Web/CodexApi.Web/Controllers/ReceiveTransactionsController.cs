c2c961f63071ae9be4cca32155b1f8c737ad91a0
George <prophet.codex@gmail.com>
2019-08-10 12:34:30 +0300

    Add ReceiveTransactionController

---
+++ src/Web/CodexApi.Web/Controllers/ReceiveTransactionsController.cs
@@ -0,0 +1,58 @@
+﻿using AutoMapper;
+using CodexApi.Services.Contracts;
+using CodexApi.Services.Models.Bank;
+using CodexApi.Web.Infrastructure.Filters;
+using CodexApi.Web.Infrastructure.Helpers;
+using CodexApi.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace CodexApi.Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [DecryptAndVerifyRequest]
+    public class ReceiveTransactionsController : ControllerBase
+    {
+        private const string BankRefusedTheRequestMessage = "{0} refused the transfer. If this error continues to occur please contact our support center.";
+        private const string BankNotFound = "{0} was not found.";
+
+        private readonly IBankService bankService;
+        private readonly CodexApiConfiguration configuration;
+
+        public ReceiveTransactionsController(IBankService bankService, IOptions<CodexApiConfiguration> configuration)
+        {
+            this.bankService = bankService;
+            this.configuration = configuration.Value;
+        }
+
+        // POST api/values
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] string data)
+        {
+            var model = JsonConvert.DeserializeObject<ReceiveTransactionModel>(data);
+            var bank = await this.bankService.GetBankAsync<BankServiceModel>(model.DestinationBankName, model.DestinationBankSwiftCode,
+                model.DestinationBankCountry);
+
+            if (bank == null)
+            {
+                return this.NotFound(string.Format(BankNotFound, model.DestinationBankName));
+            }
+
+            var sendModel = Mapper.Map<SendTransactionModel>(model);
+            var encryptedAndSignedData = TransactionHelper.SignAndEncryptData(sendModel, this.configuration.Key, bank.ApiKey);
+            var client = new HttpClient();
+            var response = await client.PostAsJsonAsync(bank.ApiAddress, encryptedAndSignedData);
+            if (response.StatusCode != HttpStatusCode.OK)
+            {
+                return this.BadRequest(string.Format(BankRefusedTheRequestMessage, model.DestinationBankName));
+            }
+
+            return this.Ok();
+        }
+    }
+}
---
+++ src/Web/CodexApi.Web/Models/SendTransactionModel.cs
@@ -0,0 +1,38 @@
+﻿using CodexBank.Common;
+using CodexBank.Common.AutoMapping.Contracts;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CodexApi.Web.Models
+{
+    public class SendTransactionModel : IMapWith<ReceiveTransactionModel>
+    {
+        [MaxLength(ModelConstants.Transaction.DescriptionMaxLength)]
+        public string Description { get; set; }
+
+        [Required]
+        public decimal Amount { get; set; }
+
+        [Required]
+        [MaxLength(ModelConstants.BankAccount.UniqueIdMaxLength)]
+        public string DestinationBankAccountUniqueId { get; set; }
+
+        [Required]
+        [MaxLength(ModelConstants.User.FullNameMaxLength)]
+        public string SenderName { get; set; }
+
+        [Required]
+        [MaxLength(ModelConstants.User.FullNameMaxLength)]
+        public string RecipientName { get; set; }
+
+        [Required]
+        [MaxLength(ModelConstants.BankAccount.UniqueIdMaxLength)]
+        public string SenderAccountUniqueId { get; set; }
+
+        [Required]
+        public string ReferenceNumber { get; set; }
+    }
+}
